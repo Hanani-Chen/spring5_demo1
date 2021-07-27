0ff78b5b0c395100a269de1bafb75db97048b822
chenhuaneng <chenhuaneng@myhexin.com>
2021-07-27 23:11:45 +0800

    xxxx xxxx xxxx 属性注入

--- src/com/atguigu/spring5/User.java
+++ src/com/atguigu/spring5/User.java
@@ -2,9 +2,15 @@ package com.atguigu.spring5;
 
 /**
  * @author hanani
- * @create 2021-07-26-1:28
+ * @created 2021-07-26-1:28
  */
 public class User {
+    private String userName;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
     public void add(){
         System.out.println("add...");
     }
--- src/com/atguigu/spring5/testdemo/TestSpring5.java
+++ src/com/atguigu/spring5/testdemo/TestSpring5.java
@@ -1,19 +1,33 @@
 package com.atguigu.spring5.testdemo;
 
+import com.atguigu.spring5.Book;
 import com.atguigu.spring5.User;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.annotation.Resource;
 
 /**
  * @author hanani
- * @create 2021-07-26-1:30
+ * @created 2021-07-26-1:30
  */
 public class TestSpring5 {
+
     @Test
     public void testAdd(){
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean1.xml");
         User user = applicationContext.getBean("user", User.class);
         System.out.println(user);
         user.add();
+    }
+
+    @Test
+    public void testBook1(){
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("bean1.xml");
+        Book book = classPathXmlApplicationContext.getBean("book", Book.class);
+        System.out.println(book);
+        book.testDemo();
     }
 }
